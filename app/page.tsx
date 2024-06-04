dfc62802f9787c3de7ad05f7ebf8a6fc8318bba3
Visa Pollari <visa.pollari@gmail.com>
2024-06-04 03:54:33 +0300

    basic

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import BaseLayout from '@/components/layout/baseLayout';
 import LoadingSpinner from '@/components/generic/loadingSpinner';
 import Button from '@/components/generic/button';
 import { client } from '@/lib/supabase';
-import { useState, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
   const { session, loading } = useAuth();
@@ -26,13 +26,41 @@ export default function Home() {
   };
 
   //get user from database if logged in
-  useCallback(async () => {
-    console.log(session);
-    if (!session) return null;
-    const user = await client.from('users').select().eq('id', session.user.id);
-    setUserFormState(user);
-    console.log(user);
-    return null;
+  useEffect(() => {
+    const getUser = async () => {
+      if (!session) {
+        console.log('no session');
+        return null;
+      }
+      const user = await client
+        .from('people')
+        .select()
+        .eq('user_id', session.user.id)
+        .single();
+      if (user.error || !user.data) {
+        const newUser = await client
+          .from('people')
+          .insert({
+            user_id: session.user.id,
+            email: session.user.email,
+            first_name: session.user.user_metadata.given_name,
+            last_name: session.user.user_metadata.family_name,
+            image_url_session: session.user.user_metadata.picture,
+          })
+          .select()
+          .single();
+        console.log('newUser', newUser);
+        if (newUser.error) {
+          throw new Error(newUser.error.message);
+        } else if (newUser.data) {
+          setUserFormState(newUser.data[0]);
+        }
+        return null;
+      }
+      setUserFormState(user.data);
+      return null;
+    };
+    getUser();
   }, [session]);
 
   const LandingPage = () => (
@@ -60,7 +88,7 @@ export default function Home() {
       {session ? (
         <BaseLayout>
           <div>
-            <h1>{userFormState.name}</h1>
+            <h1>{userFormState.first_name}</h1>
             <p>{userFormState.email}</p>
           </div>
         </BaseLayout>
