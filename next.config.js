09ece5184d4e6b2faaa70fd569be9ad4535efe69
Visa Pollari <visa.pollari@gmail.com>
2024-10-20 16:21:52 +0300

    standalon

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // For standalone serverless builds
   reactStrictMode: false,
   images: {
     remotePatterns: [
