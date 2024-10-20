4ba09497122686f1d7cf4cfaab93bf0a0159e210
Visa Pollari <visa.pollari@gmail.com>
2024-10-20 16:29:38 +0300

    external

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone', // For standalone serverless builds
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  },
   reactStrictMode: false,
   images: {
     remotePatterns: [
