2248b42f6d35e64753301ac4a6c380c6551673c7
Visa P <visa.pollari@gmail.com>
2025-02-09 19:14:13 +0200

    lint

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,7 @@ import BaseLayout from '@/components/layout/baseLayout';
 const About = () => {
   return (
     <BaseLayout className="mx-auto p-6">
-      <h1 className="font-archivo-black mb-4 text-4xl font-prompt">
-        About this app
-      </h1>
+      <h1 className="font-archivo-black mb-4 text-4xl">About this app</h1>
       <p className="font-archivo">
         For any inquiries, please email us at{' '}
         <a href="mailto:visa@opensauce.fi" className="text-blue-600">
